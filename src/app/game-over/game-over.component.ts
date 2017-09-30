22e8ea9f5d5e8ea85ed0b23ee0845bbb412d5418
maxlmo <max.moeller55@gmail.com>
2017-09-30 14:50:47 +0200

    Implement close modal.

--- src/app/game-over/game-over.component.ts
+++ src/app/game-over/game-over.component.ts
@@ -1,3 +1,4 @@
+import { BsModalRef } from 'ngx-bootstrap/modal/modal-options.class';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameOverComponent implements OnInit {
 
-  constructor() { }
+  constructor(public gameOverModalRef: BsModalRef) { }
 
   ngOnInit() {
   }
