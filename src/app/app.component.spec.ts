77089a7b6fac24ff0b0a0a9038b5e3a9087a2b93
maxlmo <max.moeller55@gmail.com>
2017-09-30 14:31:03 +0200

    Fix app component unit tests. Declare new components.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,15 @@
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SnakeComponent } from './snake/snake.component';
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        SnakeComponent,
+        NavBarComponent
       ],
     }).compileComponents();
   }));
