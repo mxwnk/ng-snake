73bce2ad6f9e292f2cd60d79ea4dee9344232834
maxlmo <max.moeller55@gmail.com>
2017-10-05 15:24:25 +0200

    Test leaderboard component.

--- src/app/leader-board/leader-board.component.spec.ts
+++ src/app/leader-board/leader-board.component.spec.ts
@@ -1,13 +1,17 @@
+import { Observable } from 'rxjs/Rx';
 import { HttpClientModule } from '@angular/common/http';
-import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject, tick, fakeAsync } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { LeaderBoardComponent } from './leader-board.component';
 import { HttpClient } from '@angular/common/http';
+import { Score } from '../model/score';
 
 describe('LeaderBoardComponent', () => {
   let component: LeaderBoardComponent;
   let fixture: ComponentFixture<LeaderBoardComponent>;
   let httpMock: HttpTestingController;
+  let firstScore: Score;
+  const scores: Score[] = [];
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -22,11 +26,24 @@ describe('LeaderBoardComponent', () => {
     fixture = TestBed.createComponent(LeaderBoardComponent);
     httpMock = TestBed.get(HttpTestingController);
     component = fixture.componentInstance;
+    firstScore = new Score();
+    firstScore.name = 'max';
+    firstScore.score = 123;
+    scores.push(firstScore);
   });
 
   it(`should get scores`, async(() => {
     component.ngOnInit();
     const request = httpMock.expectOne('http://localhost:3000/scores');
   }));
+
+  it('should save scores from httpClient', fakeAsync((inject([HttpClient], (httpClient) => {
+    spyOn(httpClient, 'get').and.returnValue(Observable.of(scores));
+
+    component.ngOnInit();
+    tick();
+
+    expect(component.scores).toBe(scores);
+  }))));
 
 });
