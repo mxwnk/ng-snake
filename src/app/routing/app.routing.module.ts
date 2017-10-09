73c4327ed94d7829a8975cd0fb087439121e877c
maxlmo <max.moeller55@gmail.com>
2017-10-09 14:44:49 +0200

    Add link and route for settings.

--- src/app/routing/app.routing.module.ts
+++ src/app/routing/app.routing.module.ts
@@ -2,10 +2,12 @@ import { SnakeComponent } from './../snake/snake.component';
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LeaderBoardComponent } from '@app/core';
+import { SettingsComponent } from '@app/core';
 
 const routes: Routes = [
     { path: '', component: SnakeComponent},
-    { path: 'leaderBoard', component: LeaderBoardComponent}
+    { path: 'leaderBoard', component: LeaderBoardComponent},
+    { path: 'settings', component: SettingsComponent}
 ];
 
 @NgModule({
